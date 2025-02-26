a67f1832d7e63c76ba088b43fbbc276a064d69aa
nesslty <pineapplekuu@gmail.com>
2025-02-26 10:03:24 +0700

    terbaru

--- Praktikum03/DataDosen15.java
+++ Praktikum03/DataDosen15.java
@@ -1,5 +1,78 @@
 package Praktikum03;
 
 public class DataDosen15 {
-    
+    public static void dataSemuaDosen(Dosen15[] arrayOfDosen) {
+        System.out.println();
+        System.out.println("..................................................................................");
+        System.out.println("DAFTAR SEMUA DOSEN:");
+        for (Dosen15 dsn : arrayOfDosen) {
+            System.out.println("Kode            : " + dsn.kode); 
+            System.out.println("Nama            : " + dsn.nama);
+            System.out.println("Jenis Kelamin   : " + (dsn.jenisKelamin ? "Pria" : "Wanita"));
+            System.out.println("Usia            : " + dsn.usia);
+            System.out.println("..................................................................................");
+            System.out.println();
+        }
+    }
+
+    public static void jumlahDosenPerJenisKelamin(Dosen15[] arrayDosen15) {
+        int pria = 0, wanita = 0;
+        for (Dosen15 dsn : arrayDosen15) {
+            if (dsn.jenisKelamin) {
+                pria++;
+            } else {
+                wanita++;
+            }
+        }
+        System.out.println();
+        System.out.println("..................................................................................");
+        System.out.println("Jumlah Dosen Pria   : " + pria + " orang");
+        System.out.println("Jumlah Dosen Wanita : " + wanita + " orang");
+        System.out.println("..................................................................................");
+    }
+
+    public static void rerataUsiaDosenPerJenisKelamin(Dosen15[] arrayDosen15) {
+        int totalUsiaPria = 0, totalUsiaWanita = 0, jumlahPria = 0, jumlahWanita = 0;
+        for (Dosen15 dsn : arrayDosen15) {
+            if (dsn.jenisKelamin) {
+                totalUsiaPria += dsn.usia;
+                jumlahPria++;
+            } else {
+                totalUsiaWanita += dsn.usia;
+                jumlahWanita++;
+            }
+        }
+
+        System.out.println("Rata-rata usia dosen Pria   : " + (jumlahPria > 0 ? (totalUsiaPria / jumlahPria) : 0) + " tahun");
+        System.out.println("Rata-rata usia dosen Wanita : " + (jumlahWanita > 0 ? (totalUsiaWanita / jumlahWanita) : 0) + " tahun");
+        System.out.println("..................................................................................");
+    }
+
+    public static void infoDosenPalingTua(Dosen15[] arrayDosen15) {
+            Dosen15 tertua = arrayDosen15[0];
+            for (Dosen15 dsn : arrayDosen15) {
+                if (dsn.usia > tertua.usia) {
+                    tertua = dsn;
+                }
+            }
+            System.out.println("DOSEN PALING TUA");
+            System.out.println("Nama Dosen: " + tertua.nama);
+            System.out.println("Umur dosen: " + tertua.usia + " tahun");
+            System.out.println("..................................................................................");
+    }
+
+    public static void infoDosenPalingMuda(Dosen15[] arrayDosen15) {
+        Dosen15 termuda = arrayDosen15[0];
+        for (Dosen15 dsn : arrayDosen15) {
+            if (dsn.usia < termuda.usia) {
+                termuda = dsn;
+            }
+        }
+        System.out.println("DOSEN PALING MUDA");
+        System.out.println("Nama Dosen: " + termuda.nama);
+        System.out.println("Umur dosen: " + termuda.usia + " tahun");
+        System.out.println("..................................................................................");
+        System.out.println();
+    }
 }
+
--- Praktikum03/DosenDemo15.java
+++ Praktikum03/DosenDemo15.java
@@ -7,21 +7,21 @@ public class DosenDemo15 {
         Dosen15[ ] arrayDosen15 = new Dosen15[3];
         String kode, dummy, nama;
         boolean jenisKelamin;
-        int usia;
+        int usia, nmr = 1;
 
         System.out.println();
 
         for (int i = 0; i < 3; i++) {
             System.out.println("__________________________________________________________________________________");
             System.out.println("Masukkan Data Dosen ke-" + (i + 1));
-            System.out.print("Kode                                          : ");
+            System.out.print("Kode                          : ");
             kode = sc.nextLine();
-            System.out.print("Nama                                          : ");
+            System.out.print("Nama                          : ");
             nama = sc.nextLine();
-            System.out.print("Jenis Kelamin (true = Pria, false = Wanita)   : ");
+            System.out.print("Jenis Kelamin (Pria/Wanita)   : ");
             dummy = sc.nextLine();
             jenisKelamin = dummy.equalsIgnoreCase("Pria");
-            System.out.print("Usia                                          : ");
+            System.out.print("Usia                          : ");
             usia = sc.nextInt();
             sc.nextLine();
 
@@ -34,18 +34,22 @@ public class DosenDemo15 {
         System.out.println();
         System.out.println("..................................................................................");
         System.out.println("RINCIAN DAFTAR DOSEN 1-3");
-        for (int i = 0; i < 3; i++) {
+        for (Dosen15 dsn : arrayDosen15) {
             System.out.println("..................................................................................");
-            System.out.println("Data Dosen " + (i + 1));
-            System.out.println("Kode            : " + arrayDosen15[i].kode);
-            System.out.println("Nama            : " + arrayDosen15[i].nama);
-            System.out.println("Jenis Kelamin   : " + (arrayDosen15[i].jenisKelamin ? "Pria" : "Wanita"));
-            System.out.println("Usia            : " + arrayDosen15[i].usia + " tahun");
+            System.out.println("Data Dosen " + nmr++);
+            System.out.println("Kode            : " + dsn.kode);
+            System.out.println("Nama            : " + dsn.nama);
+            System.out.println("Jenis Kelamin   : " + (dsn.jenisKelamin ? "Pria" : "Wanita"));
+            System.out.println("Usia            : " + dsn.usia + " tahun");
         }
         System.out.println("..................................................................................");
         System.out.println();
 
+        DataDosen15.dataSemuaDosen(arrayDosen15);
+        DataDosen15.jumlahDosenPerJenisKelamin(arrayDosen15);
+        DataDosen15.rerataUsiaDosenPerJenisKelamin(arrayDosen15);
+        DataDosen15.infoDosenPalingTua(arrayDosen15);
+        DataDosen15.infoDosenPalingMuda(arrayDosen15);
         sc.close();
     }
-    
 }
