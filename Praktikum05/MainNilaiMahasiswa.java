4df6ea8d9f0ebc31eccc04da3d529a9c0628047e
nesslty <pineapplekuu@gmail.com>
2025-03-20 05:32:25 +0700

    terbaru

--- Praktikum05/MainNilaiMahasiswa.java
+++ Praktikum05/MainNilaiMahasiswa.java
@@ -3,15 +3,15 @@ package Praktikum05;
 public class MainNilaiMahasiswa {
     public static void main(String[] args) {
         
-    int nilaiUTS [] = {78, 85, 90, 76, 92, 83, 82};
-    int nilaiUAS [] = {82, 88, 87, 79, 95, 84, 84};
+    int niuts [] = {78, 85, 90, 76, 92, 83, 82};
+    int niuas [] = {82, 88, 87, 79, 95, 84, 84};
 
-    NilaiMahasiswa mhs = new NilaiMahasiswa(nilaiUTS, nilaiUAS);
+    NilaiMahasiswa mhs = new NilaiMahasiswa(niuts, niuas);
 
-    int tertinggi = mhs.utsTinggi(0, nilaiUTS.length - 1);
+    int tertinggi = mhs.utsTinggi(0, niuts.length - 1);
     System.out.println("Nilai UTS tertinggi : " + tertinggi);
 
-    int terendah = mhs.utsRendah(0, nilaiUTS.length - 1);
+    int terendah = mhs.utsRendah(0, niuts.length - 1);
     System.out.println("Nilai UTS terendah  : " + terendah);
 
     double rrtUAS = mhs.hitungUAS();
