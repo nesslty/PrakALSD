cc2b4124122d61a50efd368ff7a0b6bbd61cd8fc
nesslty <pineapplekuu@gmail.com>
2025-03-21 16:04:17 +0700

    terbaru

--- Praktikum06/Sorting15.java
+++ Praktikum06/Sorting15.java
@@ -44,5 +44,17 @@ public class Sorting15 {
             data[min] = temp;
         }
     }
+
+    void insertionSort() {
+        for (int i = 1; i <= data.length-1; i++) {
+            int temp = data[i];
+            int j = i-1;
+            while (j >= 0 && data[j] > temp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = temp;
+        }
+    }
 }
 
--- Praktikum06/SortingMain15.java
+++ Praktikum06/SortingMain15.java
@@ -18,5 +18,14 @@ public class SortingMain15 {
     dataurut2.SelectionSort();
     System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
     dataurut2.tampil();
-    }   
+
+    int c[] = {40, 10, 4, 9, 3};
+    Sorting15 dataurut3 = new Sorting15(c, c.length);
+
+    System.out.println("Data awal 3");
+    dataurut3.tampil();
+    dataurut3.insertionSort();
+    System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+    dataurut3.tampil();
+    }
 }
