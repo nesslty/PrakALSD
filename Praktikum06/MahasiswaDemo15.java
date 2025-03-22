c0cc12fdeb074d01d4cb02a680677ac7d54da8b5
nesslty <pineapplekuu@gmail.com>
2025-03-22 19:32:23 +0700

    terbaru

--- Praktikum06/MahasiswaDemo15.java
+++ Praktikum06/MahasiswaDemo15.java
@@ -23,5 +23,9 @@ public class MahasiswaDemo15 {
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC) ");
         list.selectionSort();
         list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC) ");
+        list.insertionSort();
+        list.tampil();
     }
 }
