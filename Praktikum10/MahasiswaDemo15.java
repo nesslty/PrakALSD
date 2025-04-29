823835f67a984dacdaf5c056abaa6bb45810f6ab
nesslty <pineapplekuu@gmail.com>
2025-04-30 01:24:02 +0700

    praktikum 2

--- Praktikum10/MahasiswaDemo15.java
+++ Praktikum10/MahasiswaDemo15.java
@@ -37,6 +37,8 @@ public class MahasiswaDemo15 {
                         int nilai = scan.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai biner tugas: " + biner);
                     }
                     break;
                 case 3:
