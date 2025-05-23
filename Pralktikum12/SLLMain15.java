be4eb6ec9e1e06d10933a08a8263e63f010800f1
nesslty <pineapplekuu@gmail.com>
2025-05-23 09:56:56 +0700

    praktikum 2

--- Pralktikum12/SLLMain15.java
+++ Pralktikum12/SLLMain15.java
@@ -24,8 +24,19 @@ public class SLLMain15 {
 
             Mahasiswa15 mhsNew = new Mahasiswa15(nim, nama, kelas, ipk);
             sll.addLast(mhsNew);
-        sll.print();
         }
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
         sc.close();
     }
 }
